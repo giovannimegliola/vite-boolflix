3c1c68aa99f1c99ac31a3a419343abdbeff3e74e
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-20 13:06:54 +0100

    modified Main titles style

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -2,14 +2,14 @@
   <main>
     <div class="container">
       <div class="row justify-content-center">
-        <h2>Film</h2>
+        <h2 style="color: white;">Film</h2>
       <div v-if="store.errorMessage" class="col-6 mt-5 mb-2 text-center error-text">
         <h2>{{ store.errorMessage }}</h2>
       </div>
       <div v-else v-for="(movie, index) in store.moviesList" :key="movie.id" class="col-6 col-md-4 mt-5 mb-2 text-center">
         <CardMoviesApp v-bind:myMovie="movie" @mouseover="rotate[index] = true" @mouseout="rotate[index] = false" :class="{ rotate: rotate[index] }"/>
       </div>
-      <h2>Serie Tv</h2>
+      <h2 style="color: white;" >Serie Tv</h2>
       <div v-for="(serie, index) in store.seriesList" :key="serie.id" class="col-6 col-md-4 mt-5 mb-2 text-center">
         <CardSeriesApp v-bind:mySerie="serie" @mouseover="rotate[index] = true" @mouseout="rotate[index] = false" :class="{ rotate: rotate[index] }"/>
       </div>
